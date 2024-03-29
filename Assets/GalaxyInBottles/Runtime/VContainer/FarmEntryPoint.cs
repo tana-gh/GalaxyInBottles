a7d98531565e3fe8664fdf3cad1cad8337e59a84
tana-gh <yozoranotakaku@gmail.com>
2024-03-30 08:33:53 +0900

    feat: SceneとLifetimeの雛形を追加

---
+++ Assets/GalaxyInBottles/Runtime/VContainer/FarmEntryPoint.cs
@@ -0,0 +1,28 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MessagePipe;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace tana_gh.GalaxyInBottles
+{
+    public partial class FarmEntryPoint : IAsyncStartable, ITickable
+    {
+        //[Inject] private readonly IPublisher<UpdateEvent> _updatePub;
+
+        //partial void Init();
+
+        public async UniTask StartAsync(CancellationToken cancellationToken)
+        {
+            //Init();
+
+            //await UniTask.Yield();
+        }
+
+        public void Tick()
+        {
+            //_updatePub.Publish(new UpdateEvent(Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/GalaxyInBottles/Runtime/VContainer/RootLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace tana_gh.GalaxyInBottles
+{
+    public class RootLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
