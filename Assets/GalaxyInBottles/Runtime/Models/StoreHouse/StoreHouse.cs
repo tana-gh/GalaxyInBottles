fd14d125f229a9b864f389448129c1c7e4fdd753
tana-gh <yozoranotakaku@gmail.com>
2024-04-09 18:05:33 +0900

    chore: StoreHouseの雛形を追加

---
+++ Assets/GalaxyInBottles/Runtime/Models/StoreHouse/StoreHouse.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tana_gh.GalaxyInBottles
+{
+    [Serializable]
+    public class StoreHouse
+    {
+        public int coins;
+        public ItemStack[] itemStacks;
+    }
+}
